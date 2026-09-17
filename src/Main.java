9dc6f5212a1c221c42aa162168363cf19f25e9d9
Reilly Martinez <martinezguerrr1021@students.bowiestate.edu>
2026-09-17 10:22:09 -0400

    Inheritance-2

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,15 @@
 
 
-public class Main {
-    public static void main(String[] args) {
-        // Create two person object
-        Person p1 = new Person();
+        public class Main {
+            public static void main(String[] args) {
+                // Create two person object
+                Person p1 = new Person();
 
-        Person p2 = new Person(72, 70, "Back", "Barack Obama", 170 );
+//        Person p2 = new Person(72, 70, "Back", "Barack Obama", 170 );
+//        System.out.println(p2.name);
+                Person p2 = new Person(72, 70 );
+                System.out.println(p2.name);
 
-        System.out.println(p2.name);
+                Person p3 = p2.Call_Priavte();
     }
 }
--- src/Person.java
+++ src/Person.java
@@ -1,3 +1,4 @@
+// Private and protected are not allowed in here
 public class Person {
     // height, age, color, name, weight: instance variable
     double height;
@@ -6,7 +7,8 @@ public class Person {
     String name;
     double weight;
 
-    Person(){
+    // Constructors can have private, protected, public access modifiers
+    public Person() {
         // Defualt constructors can be empty
         height = 48;
         age = 80;
@@ -15,14 +17,29 @@ public class Person {
         weight = 140;
     }
 
+    // Protected members are accessible within the same package - java files under same directory/ folder
+    protected Person(double height, int age) {
+        // Parameters are local variables to the constructor
+        this.height = height;
+        this.age = age;
+    }
+
+
     // Overloaded Constructor (We cannot have two defualt constructors)
-    Person(double height, int age, String color, String name, double weight){
+    private Person(double height, int age, String color, String name, double weight) {
         // Parameters are local variables to the constructor
         this.height = height;
         this.age = age;
         this.color = color;
         this.name = name;
         this.weight = weight;
+    }
+
+
+
+    public Person Call_Priavte() {
+        Person p3 = new Person(72, 70, "Back", "Barack Obama", 170);
+        return p3;
     }
 }
 
